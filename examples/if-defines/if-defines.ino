13e70d266889218e344289bb98117ef001a3d82a
Jeremy Ellis <keyfreemusic@gmail.com>
2023-09-18 14:28:07 -0700

    Create if-defines.ino

---
+++ examples/if-defines/if-defines.ino
@@ -0,0 +1,47 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+#if defined(ARDUINO)  
+  Serial.println("Your company is defined as ARDUINO");
+#endif
+
+#if defined(YOUR_BOARD1)    
+  Serial.println("Your company is defined as YOUR_BOARD1");
+#endif
+
+  Serial.println("Companies Done!");
+  Serial.println("");
+
+
+#if defined(ARDUINO_ARDUINO_NANO33BLE)   ||  defined (YOUR_BOARD1) 
+  Serial.println("Your board is defined as ARDUINO_ARDUINO_NANO33BLE");
+#endif
+
+#if defined(ARDUINO_PORTENTA_H7_M7)   ||  defined (YOUR_BOARD1) 
+  Serial.println("Your board is defined as ARDUINO_PORTENTA_H7_M7");
+#endif
+
+#if defined(CORE_CM7)   ||  defined (YOUR_BOARD1) 
+  Serial.println("Your board is defined as CORE_CM7");
+#endif
+
+#if defined(ARDUINO_NICLA_VISION)   ||  defined (YOUR_BOARD1) 
+  Serial.println("Your board is defined as ARDUINO_NICLA_VISION");
+#endif
+
+#if defined(SEEED_XIAO_M0)   ||  defined (YOUR_BOARD1) 
+  Serial.println("Your board is defined as SEEED_XIAO_M0");
+#endif
+
+#if defined(SEEED_XIAO_ESP3S3)   ||  defined (YOUR_BOARD1) 
+  Serial.println("Your board is defined as SEEED_XIAO_ESP3S3");
+#endif
+
+  Serial.println("Boards Done");
+  Serial.println("---------------------");
+  delay(1000);
+
+}
