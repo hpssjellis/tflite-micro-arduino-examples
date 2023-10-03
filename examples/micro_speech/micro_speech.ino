4f9036dcdafab9b5cc823b1087c18e4317fa4a30
Jeremy Ellis <keyfreemusic@gmail.com>
2023-10-02 18:41:02 -0700

    Update micro_speech.ino

--- examples/micro_speech/micro_speech.ino
+++ examples/micro_speech/micro_speech.ino
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <TensorFlowLite.h>
+#include <TensorFlowLiteRocksetta2023.h>
 
 #include "audio_provider.h"
 #include "command_responder.h"
